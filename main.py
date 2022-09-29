6645ffdac3a47be043b3a98c5ca561850861dd7e
mtm1996 <84542027+mtm1996@users.noreply.github.com>
2022-09-29 16:36:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,31 @@ from socket import *
 import sys # In order to terminate the program
 
 serverSocket = socket ( AF_INET , SOCK_STREAM )
-# Prepare a server socket on a particular port
-# Fill in code to set up the port
+serverPort = 12000 # Prepare a server socket on a particular port
+ 	# Fill in code to set up the port
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
 while True :
 # Establish the connection
 	print (' Ready to serve ...')
-	connectionSocket , addr = serverSocket.accept()
+	connectionSocket , addr = serverSocket.accept() # Fill in code to get a connection
 	try :
-		message = # Fill in code to read GET request
+		message = connectionSocket.recv(1024).decode() # Fill in code to read GET request
 		filename = message . split () [1]
 		# Fill in security code
 		f = open ( filename )
 		outputdata = # Fill in code to read data from the file
 		# Send HTTP header line ( s ) into socket
-		# Fill in code to send header ( s )
+		connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n')# Fill in code to send header ( s )
 		# Send the content of the requested file to the client
 		for i in range (0 , len ( outputdata ) ) :
 			connectionSocket . send ( outputdata [ i ]. encode () )
-		connectionSocket . send ( " \ r \ n " . encode () )
-		connectionSocket . close ()
+		connectionSocket.send ( " \ r \ n " . encode () )
+		connectionSocket.close ()
 	except IOError :
-		# Send response message for file not found
+		connectionSocket.send('HTTP/1.1 404 Not Found\r\n\r\n')# Send response message for file not found
 		# Fill in
-		# Close client socket
+		connectionSocket.close()# Close client socket
 		# Fill in
-serverSocket.close()
+serverSocket . close ()
 sys . exit () # Terminate the program after sending the corresponding data
